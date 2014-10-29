30245573952aec2406a0dc68a41c393c6375ee97
Donnerbart <github@thunderphreak.de>
2014-10-29 05:29:53 +0100

    Created base class for unit tests.

---
+++ inazuma-storage-cluster-service/src/test/java/de/donnerbart/inazuma/storage/cluster/storage/BaseUnitTest.java
@@ -0,0 +1,77 @@
+package de.donnerbart.inazuma.storage.cluster.storage;
+
+import com.couchbase.client.core.BackpressureException;
+import de.donnerbart.inazuma.storage.base.stats.StatisticManager;
+import de.donnerbart.inazuma.storage.cluster.storage.metadata.DocumentMetadata;
+import de.donnerbart.inazuma.storage.cluster.storage.wrapper.GsonWrapper;
+import org.testng.annotations.AfterSuite;
+import rx.Observable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseUnitTest
+{
+	static final Observable<String> DATABASE_GET_RESPONSE_SUCCESS = Observable.just(null);
+	static final Observable<String> DATABASE_GET_RESPONSE_FAILURE = Observable.error(new BackpressureException());
+	static final Observable<Boolean> DATABASE_RESPONSE_SUCCESS = Observable.just(Boolean.TRUE);
+	static final Observable<Boolean> DATABASE_RESPONSE_FAILURE = Observable.error(new BackpressureException());
+
+	final static String ANY_USER_1 = "1000000";
+	final static String DOCUMENT_METADATA_KEY_USER_1 = "u-" + ANY_USER_1;
+	final static String ANY_USER_2 = "2000000";
+	final static String DOCUMENT_METADATA_KEY_USER_2 = "u-" + ANY_USER_2;
+
+	final static String DOCUMENT_METADATA_JSON_EMPTY = "{}";
+
+	final static String DOCUMENT_1_KEY = "11111111-e786-485e-963c-9a613b0368fe";
+	final static String DOCUMENT_1_JSON = "{content:\"mail1\"}";
+	final static long DOCUMENT_1_CREATED = 123456789;
+	final static DocumentMetadata DOCUMENT_1_METADATA = new DocumentMetadata(DOCUMENT_1_CREATED, false);
+
+	final static String DOCUMENT_2_KEY = "22222222-e786-485e-963c-9a613b0368fe";
+	final static String DOCUMENT_2_JSON = "{content:\"mail2\"}";
+	final static long DOCUMENT_2_CREATED = 234567891;
+	final static DocumentMetadata DOCUMENT_2_METADATA = new DocumentMetadata(DOCUMENT_2_CREATED, false);
+
+	final static String DOCUMENT_3_KEY = "33333333-e786-485e-963c-9a613b0368fe";
+	final static String DOCUMENT_3_JSON = "{content:\"mail3\"}";
+	final static long DOCUMENT_3_CREATED = 345678912;
+	final static DocumentMetadata DOCUMENT_3_METADATA = new DocumentMetadata(DOCUMENT_3_CREATED, false);
+
+	final static String DOCUMENT_METADATA_JSON_1;
+	final static String DOCUMENT_METADATA_JSON_2;
+	final static String DOCUMENT_METADATA_JSON_3;
+	final static String DOCUMENT_METADATA_JSON_1_AND_2;
+	final static String DOCUMENT_METADATA_JSON_2_AFTER_1;
+
+	static
+	{
+		final Map<String, DocumentMetadata> documentMetadataMap1 = new HashMap<>();
+		documentMetadataMap1.put(DOCUMENT_1_KEY, DOCUMENT_1_METADATA);
+		DOCUMENT_METADATA_JSON_1 = GsonWrapper.toJson(documentMetadataMap1);
+
+		final Map<String, DocumentMetadata> documentMetadataMap2 = new HashMap<>();
+		documentMetadataMap2.put(DOCUMENT_2_KEY, DOCUMENT_2_METADATA);
+		DOCUMENT_METADATA_JSON_2 = GsonWrapper.toJson(documentMetadataMap2);
+
+		final Map<String, DocumentMetadata> documentMetadataMap3 = new HashMap<>();
+		documentMetadataMap3.put(DOCUMENT_3_KEY, DOCUMENT_3_METADATA);
+		DOCUMENT_METADATA_JSON_3 = GsonWrapper.toJson(documentMetadataMap3);
+
+		final Map<String, DocumentMetadata> documentMetadataMap1And2 = new HashMap<>();
+		documentMetadataMap1And2.put(DOCUMENT_1_KEY, DOCUMENT_1_METADATA);
+		documentMetadataMap1And2.put(DOCUMENT_2_KEY, DOCUMENT_2_METADATA);
+		DOCUMENT_METADATA_JSON_1_AND_2 = GsonWrapper.toJson(documentMetadataMap1And2);
+
+		final Map<String, DocumentMetadata> documentMetadataMap2After1 = GsonWrapper.getDocumentMetadataMap(DOCUMENT_METADATA_JSON_1);
+		documentMetadataMap2After1.put(DOCUMENT_2_KEY, DOCUMENT_2_METADATA);
+		DOCUMENT_METADATA_JSON_2_AFTER_1 = GsonWrapper.toJson(documentMetadataMap2After1);
+	}
+
+	@AfterSuite
+	public void tearDown()
+	{
+		StatisticManager.getInstance().shutdown();
+	}
+}
--- inazuma-storage-cluster-service/src/test/java/de/donnerbart/inazuma/storage/cluster/storage/StorageControllerAddDocumentTest.java
+++ inazuma-storage-cluster-service/src/test/java/de/donnerbart/inazuma/storage/cluster/storage/StorageControllerAddDocumentTest.java
@@ -1,79 +1,16 @@
 package de.donnerbart.inazuma.storage.cluster.storage;
 
-import com.couchbase.client.core.BackpressureException;
-import de.donnerbart.inazuma.storage.base.stats.StatisticManager;
-import de.donnerbart.inazuma.storage.cluster.storage.metadata.DocumentMetadata;
 import de.donnerbart.inazuma.storage.cluster.storage.wrapper.DatabaseWrapper;
-import de.donnerbart.inazuma.storage.cluster.storage.wrapper.GsonWrapper;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import rx.Observable;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.mockito.Mockito.*;
 
-public class StorageControllerAddDocumentTest
+public class StorageControllerAddDocumentTest extends BaseUnitTest
 {
-	private static final Observable<String> DATABASE_GET_RESPONSE_SUCCESS = Observable.just(null);
-	private static final Observable<String> DATABASE_GET_RESPONSE_FAILURE = Observable.error(new BackpressureException());
-	private static final Observable<Boolean> DATABASE_RESPONSE_SUCCESS = Observable.just(Boolean.TRUE);
-	private static final Observable<Boolean> DATABASE_RESPONSE_FAILURE = Observable.error(new BackpressureException());
-
-	private final static String ANY_USER_1 = "1000000";
-	private final static String DOCUMENT_METADATA_KEY_USER_1 = "u-" + ANY_USER_1;
-	private final static String ANY_USER_2 = "2000000";
-	private final static String DOCUMENT_METADATA_KEY_USER_2 = "u-" + ANY_USER_2;
-
-	private final static String DOCUMENT_1_KEY = "11111111-e786-485e-963c-9a613b0368fe";
-	private final static String DOCUMENT_1_JSON = "{content:\"mail1\"}";
-	private final static long DOCUMENT_1_CREATED = 123456789;
-	private final static DocumentMetadata DOCUMENT_1_METADATA = new DocumentMetadata(DOCUMENT_1_CREATED, false);
-
-	private final static String DOCUMENT_2_KEY = "22222222-e786-485e-963c-9a613b0368fe";
-	private final static String DOCUMENT_2_JSON = "{content:\"mail2\"}";
-	private final static long DOCUMENT_2_CREATED = 234567891;
-	private final static DocumentMetadata DOCUMENT_2_METADATA = new DocumentMetadata(DOCUMENT_2_CREATED, false);
-
-	private final static String DOCUMENT_3_KEY = "33333333-e786-485e-963c-9a613b0368fe";
-	private final static String DOCUMENT_3_JSON = "{content:\"mail3\"}";
-	private final static long DOCUMENT_3_CREATED = 345678912;
-	private final static DocumentMetadata DOCUMENT_3_METADATA = new DocumentMetadata(DOCUMENT_3_CREATED, false);
-
-	private final static String DOCUMENT_METADATA_JSON_1;
-	private final static String DOCUMENT_METADATA_JSON_2;
-	private final static String DOCUMENT_METADATA_JSON_3;
-	private final static String DOCUMENT_METADATA_JSON_1_AND_2;
-	private final static String DOCUMENT_METADATA_JSON_2_AFTER_1;
-
-	static
-	{
-		final Map<String, DocumentMetadata> documentMetadataMap1 = new HashMap<>();
-		documentMetadataMap1.put(DOCUMENT_1_KEY, DOCUMENT_1_METADATA);
-		DOCUMENT_METADATA_JSON_1 = GsonWrapper.toJson(documentMetadataMap1);
-
-		final Map<String, DocumentMetadata> documentMetadataMap2 = new HashMap<>();
-		documentMetadataMap2.put(DOCUMENT_2_KEY, DOCUMENT_2_METADATA);
-		DOCUMENT_METADATA_JSON_2 = GsonWrapper.toJson(documentMetadataMap2);
-
-		final Map<String, DocumentMetadata> documentMetadataMap3 = new HashMap<>();
-		documentMetadataMap3.put(DOCUMENT_3_KEY, DOCUMENT_3_METADATA);
-		DOCUMENT_METADATA_JSON_3 = GsonWrapper.toJson(documentMetadataMap3);
-
-		final Map<String, DocumentMetadata> documentMetadataMap1And2 = new HashMap<>();
-		documentMetadataMap1And2.put(DOCUMENT_1_KEY, DOCUMENT_1_METADATA);
-		documentMetadataMap1And2.put(DOCUMENT_2_KEY, DOCUMENT_2_METADATA);
-		DOCUMENT_METADATA_JSON_1_AND_2 = GsonWrapper.toJson(documentMetadataMap1And2);
-
-		final Map<String, DocumentMetadata> documentMetadataMap2After1 = GsonWrapper.getDocumentMetadataMap(DOCUMENT_METADATA_JSON_1);
-		documentMetadataMap2After1.put(DOCUMENT_2_KEY, DOCUMENT_2_METADATA);
-		DOCUMENT_METADATA_JSON_2_AFTER_1 = GsonWrapper.toJson(documentMetadataMap2After1);
-	}
-
 	@Mock
 	private DatabaseWrapper databaseWrapper;
 
@@ -91,12 +28,6 @@ public class StorageControllerAddDocumentTest
 
 		databaseFailOnceAnswer = new DatabaseFailureMultipleTimesAnswer<>(DATABASE_RESPONSE_SUCCESS, DATABASE_RESPONSE_FAILURE);
 		databaseGetFailOnceAnswer = new DatabaseFailureMultipleTimesAnswer<>(DATABASE_GET_RESPONSE_SUCCESS, DATABASE_GET_RESPONSE_FAILURE);
-	}
-
-	@AfterMethod
-	public void tearDown()
-	{
-		StatisticManager.getInstance().shutdown();
 	}
 
 	@Test
